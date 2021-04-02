37dd7b819ce0368394dbf3a3b1416abc02ad7133
dekken <philip.deegan@gmail.com>
2021-04-02 12:16:35 +0200

    updates

--- mkn.sh
+++ mkn.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
-set -e
-THREADS=${THREADS:=""}
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DIR="caliper"
-GIT_URL="https://github.com/llnl/$DIR"
-VERSION="master"
-FFF=("include" "lib" "$DIR" "share")
-[ ! -z "$MKN_CLEAN" ] && (( $MKN_CLEAN == 1 )) && for f in ${FFF[@]}; do rm -rf $CWD/$f; done
-[ ! -d "$CWD/$DIR" ] && git clone --depth 1 $GIT_URL -b $VERSION $DIR --recursive
-cd $CWD/$DIR
-rm -rf build && mkdir build && cd build
 
-cmake .. \
-  -DENABLE_OPENMP=OFF \
-  -DCMAKE_INSTALL_PREFIX=$CWD \
-  -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true \
-  -DCMAKE_CXX_FLAGS="-g0 -O3 -march=native -mtune=native" \
-  -DCMAKE_BUILD_TYPE=Release \
+exec 19>$CWD/.mkn.sh.cmd # set -x redirect
+export BASH_XTRACEFD=19  # set -x redirect
 
-make VERBOSE=1 -j$THREADS && make install
-cd .. && rm -rf build
+set -xe
+(
+  date
+
+  THREADS=${THREADS:=""}
+  DIR="caliper"
+  GIT_URL="https://github.com/llnl/$DIR"
+  VERSION="master"
+  FFF=("include" "lib" "$DIR" "share")
+  [ ! -z "$MKN_CLEAN" ] && (( $MKN_CLEAN == 1 )) && for f in ${FFF[@]}; do rm -rf $CWD/$f; done
+  [ ! -d "$CWD/$DIR" ] && git clone --depth 1 $GIT_URL -b $VERSION $DIR --recursive
+
+  cd $CWD/$DIR
+
+  rm -rf build && mkdir build && cd build
+
+  cmake \
+    -DCMAKE_INSTALL_PREFIX=$CWD                             \
+    -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true             \
+    -DCMAKE_CXX_FLAGS="-g0 -O3 -march=native -mtune=native" \
+    -DWITH_MPI=ON                                           \
+    -DCMAKE_BUILD_TYPE=Release                              \
+    ..
+  make VERBOSE=1 -j$THREADS && make install
+  cd .. && rm -rf build
+  find $CWD -maxdepth 1 -size 0 -name ".mkn.sh.*" -delete
+  date
+) 1> >(tee $CWD/.mkn.sh.out ) 2> >(tee $CWD/.mkn.sh.err >&2 )
